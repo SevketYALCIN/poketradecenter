3d735b5246d5254e2a3e1921bd461dd3faee2100
SevketYALCIN <sevket.yalcin@outlook.com>
2018-11-21 17:39:47 +0900

    Config: Use env variable for connection string

--- PokeTradeCenter/Startup.cs
+++ PokeTradeCenter/Startup.cs
@@ -37,7 +37,7 @@ namespace PokeTradeCenter
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<PokeTradeCenterContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("PokeTradeCenterContext")));
+                    options.UseSqlServer(Environment.GetEnvironmentVariable("ASPNETCORE_PokeTradeCenterConnectionString")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
