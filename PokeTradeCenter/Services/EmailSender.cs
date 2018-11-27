2f87bd586e3ddc6c7cf4c6534741297147287d56
SevketYALCIN <sevket.yalcin@outlook.com>
2018-11-27 18:00:16 +0900

    feature: add sendgrid + email verifications

---
+++ PokeTradeCenter/Services/EmailSender.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Threading.Tasks;
+
+namespace PokeTradeCenter.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            return Execute(Environment.GetEnvironmentVariable("ASPNETCORE_SendGridKey"), subject, message, email);
+        }
+
+        public Task Execute(string apiKey, string subject, string message, string email)
+        {
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress("poketradecenter@outlook.com", "PokeTradeCenter"),
+                Subject = subject,
+                PlainTextContent = message,
+                HtmlContent = message
+            };
+            msg.AddTo(new EmailAddress(email));
+
+            // Disable click tracking.
+            // See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
+            msg.SetClickTracking(false, false);
+
+            return client.SendEmailAsync(msg);
+        }
+    }
+}
